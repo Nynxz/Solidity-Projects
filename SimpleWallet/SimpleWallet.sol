dff2c0c87e22f330c47d63ad2b89b4403ca95c84
Henry Lee <nynxzx@gmail.com>
2021-04-02 21:30:49 +1000

    Seperation of Contracts

--- SimpleWallet/SimpleWallet.sol
+++ SimpleWallet/SimpleWallet.sol
@@ -1,30 +1,7 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-
-contract Allowance is Ownable{
-    using SafeMath for uint;
-    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
-    
-    mapping(address => uint) public allowance;
-    
-    function addAllowance(address _who, uint _amount) public onlyOwner {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-        allowance[_who] = allowance[_who].add(_amount);
-    }
-
-    modifier ownerOrAllowed(uint _amount) {
-        require(owner() == _msgSender() || allowance[msg.sender] >= _amount, "You are not allowed");
-        _;
-    }
-    
-    function reduceAllowance(address _who, uint _amount) internal {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
-        allowance[_who] = allowance[_who].sub(_amount);
-    }
-    
-}
+import "./Allowance.sol";
 
 contract SimpleWallet is Allowance {
     
@@ -37,18 +14,22 @@ contract SimpleWallet is Allowance {
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         require(_amount <= address(this).balance, "There is not enough funds in the smart contract");
-        if( !(owner() == _msgSender()) ){ // Owner is not losing allowance on withdraw
-            reduceAllowance(_msgSender(), _amount);
-        }
+        //if( !(owner() == _msgSender()) ){ // Owner is not losing allowance on withdraw
+        reduceAllowance(_msgSender(), _amount);
+        //}
         emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
     
-    function renounceOwnership() public override onlyOwner {
+    function renounceOwnership() public view override onlyOwner {
         revert("Can't Renounce Ownership.");
     }
 
     fallback () external payable {
+        emit MoneyRecieved(msg.sender, msg.value);
+        addAllowance(msg.sender, msg.value);
+    }
+    receive() external payable {
         emit MoneyRecieved(msg.sender, msg.value);
         addAllowance(msg.sender, msg.value);
     }
